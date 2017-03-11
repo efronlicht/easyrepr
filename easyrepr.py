ddc9bdeeab6e7ed96c6cc570c2181b3d5ab97174
Efron Licht <efron.python@gmail.com>
2017-03-11 09:49:33 -0800

    removal of instance __repr__ override. new tests.

--- easyrepr.py
+++ easyrepr.py
@@ -9,7 +9,7 @@ from itertools import chain
 
 def easyrepr(cls):
     """class decorator. tries to create self-documenting __repr__. 
-    such thast, where possible for immutable objects with 
+    such that, where possible for immutable objects with 
     implemented __equals__, 
     eval(repr(x)) == x 
     
@@ -23,7 +23,6 @@ def easyrepr(cls):
         kwargstr = (f'{arg} = {kwargs[arg]!r}' for arg in kwargs)
         args = ', '.join(chain(argstr, kwargstr))
         instance._easyrepr = f'{cls.__name__}({args})'
-        instance.__repr__ = _repr
         return instance
     cls.__new__ = _new
     cls.__repr__ = _repr
@@ -37,8 +36,35 @@ if __name__ == '__main__':
             self.foo, self.bar = foo, bar
         def __eq__(self, other):
             return self.foo, self.bar == other.foo, other.bar
-    a = TestClass('foo', bar = 'bar')
-    b = TestClass('spam', bar = 'eggs')
-    c = TestClass(a, bar = b)
-    assert repr(c) == "TestClass(TestClass('foo', bar = 'bar'), bar = TestClass('spam', bar = 'eggs'))"
-    assert c == eval(repr(c))
+    class OtherTestClass(TestClass):
+        pass
+    class OverRiddenRepr(TestClass):
+        def __repr__(self):
+            return 'override'
+    
+    def test_simple_repr():
+        a = TestClass('foo', bar = 'bar')
+        assert repr(a) == "TestClass('foo', bar = 'bar')"
+    def test_subclass_repr():
+        assert repr(OtherTestClass('foo', bar = 'bar')) == "OtherTestClass('foo', bar = 'bar')"
+    def test_recursive_repr():
+        a = TestClass('foo', bar = 'bar')
+        b = TestClass('spam', bar = 'eggs')
+        c = TestClass(a, bar = b)
+        assert repr(c) == "TestClass(TestClass('foo', bar = 'bar'), bar = TestClass('spam', bar = 'eggs'))"
+        assert c == eval(repr(c))
+    def test_repr_override():
+        assert repr(OverRiddenRepr('foo', bar = 'bar')) == 'override'
+            
+    tests = [test_recursive_repr, test_simple_repr, test_subclass_repr, test_repr_override]
+    passed = True
+    for test in tests:
+        try:
+            test()
+        except AssertionError:
+            print(f'{test.__name__} failed!')
+            passed = False
+        else:
+            print(f'{test.__name__} passed!')
+    if passed:
+        print('all tests passed!')
